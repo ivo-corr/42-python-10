f1211709e3d98b974b2f66385d13b45e3005f2c8
ivo <icorrale@student.42heilbronn.de>
2026-07-12 13:50:06 +0200

    a nice partial_enchanter function

--- ex3/functools_artifacts.py
+++ ex3/functools_artifacts.py
@@ -1,5 +1,5 @@
 from typing import Any, Callable
-from functools import reduce
+from functools import reduce, partial, lru_cache
 import operator
 
 
@@ -15,7 +15,11 @@ def spell_reducer(spells: list[int], operation: str) -> int:
 
 
 def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
-    pass
+    ps: dict[str, Callable] = {}
+    ps.update({"1": partial(base_enchantment, power=50, element="Fire")})
+    ps.update({"2": partial(base_enchantment, power=50, element="Water")})
+    ps.update({"3": partial(base_enchantment, power=50, element="Air")})
+    return ps
 
 
 def memoized_fibonacci(n: int) -> int:
@@ -27,12 +31,24 @@ def spell_dispatcher() -> Callable[[Any], str]:
 
 
 def main() -> None:
-    data = [[40, 30, 20, 10]]
+    data = [[40, 30, 20, 10],
+            [f := lambda power, element, target: (
+                f'Power={power}, Element={element}, Target={target}'),
+             partial_enchanter(f)]
+]
     print("\nTesting spell reducer...")
     print(f"Sum: {spell_reducer(data[0], 'add')}")
     print(f"Product: {spell_reducer(data[0], 'multiply')}")
     print(f"Max: {spell_reducer(data[0], 'max')}")
     # print(f"Min: {spell_reducer(data[0], 'min')}")
+    print("\nTesting partial enchanter...")
+    element: str = "Earth"
+    target: str = "Warrior"
+    print("Base function app: "
+          f"{data[1][0](40, element, target)}")
+    [print("Partial function app: "
+           f"{data[1][1][k](target=target)}") for k in data[1][1].keys()]
+
     print("\nTesting memoized fibonacci...")
 
 
